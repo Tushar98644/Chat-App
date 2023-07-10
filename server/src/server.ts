4e0550dc410a86c13e41a9539d3ba91d0318c8cd
Tushar98644 <b.toshar@iitg.ac.in>
2023-07-10 11:45:46 +0530

    user routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import connectDB from './config/db';
+import connectDB from '../config/db';
+import UserRoutes from '../routes/UserRoutes';
 
 const app = express();
 dotenv.config();
@@ -8,5 +9,7 @@ connectDB();
 
 app.listen(process.env.PORT, () => {
     console.log(`🚀 Server started on port ${process.env.PORT}`);
-    }   
+    }  
 );
+
+app.use('/api/user',UserRoutes)
